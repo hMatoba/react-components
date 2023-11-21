3e1e8e5dcea0c5473d3702f725f3b5b7b69e0ea0
Hiroaki Matoba <hiroaki.mtb@outlook.com>
2023-11-21 17:43:25 +0900

    SwtichのcheckboxではpropsのdefaultCheckedを使う

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -82,7 +82,7 @@ export const Switch: React.FC<Props> = ({ on, disabled, onClick }) => {
           <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
         </svg>
       </div>
-      <input type="checkbox" checked={on} hidden />
+      <input type="checkbox" defaultChecked={on} hidden />
     </button>
   );
 };
